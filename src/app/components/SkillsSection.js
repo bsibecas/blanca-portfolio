9fc1ccd5fccd098a5326221057580a837b8aeb72
bsibecas <blanca.sibecas-hernandez@epitech.eu>
2026-07-22 16:50:08 -0300

    [ADD] centralize portfolio assets

--- src/app/components/SkillsSection.js
+++ src/app/components/SkillsSection.js
@@ -1,15 +1,15 @@
 // components/SkillsSection.jsx
 import React from 'react';
 import Image from 'next/image';
-import nextLogo from '../../../public/Logos/next-logo.png';
-import reactLogo from '../../../public/Logos/react-logo.svg.png';
-import pythonLogo from '../../../public/Logos/python-logo.svg';
-import pytorchLogo from '../../../public/Logos/pytorch-logo.webp';
-import cLogo from '../../../public/Logos/c-logo.png';
-import tailwindLogo from '../../../public/Logos/tailwind-logo.png';
-import firebaseLogo from '../../../public/Logos/firebase-logo.png';
-import nodeLogo from '../../../public/Logos/node-logo.png';
-import javascriptLogo from '../../../public/Logos/javascript-logo.webp';
+import nextLogo from '../../../public/assets/logos/next-logo.png';
+import reactLogo from '../../../public/assets/logos/react-logo.svg.png';
+import pythonLogo from '../../../public/assets/logos/python-logo.svg';
+import pytorchLogo from '../../../public/assets/logos/pytorch-logo.webp';
+import cLogo from '../../../public/assets/logos/c-logo.png';
+import tailwindLogo from '../../../public/assets/logos/tailwind-logo.png';
+import firebaseLogo from '../../../public/assets/logos/firebase-logo.png';
+import nodeLogo from '../../../public/assets/logos/node-logo.png';
+import javascriptLogo from '../../../public/assets/logos/javascript-logo.webp';
 
 
 const skills = [
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/assets/brand/favicon.ico" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
